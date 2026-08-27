2eee8fad24c26525b0c06758bd4be9db6636046f
unknown <saman.balahang.2012@gmail.com>
2026-08-27 08:28:25 +0330

    hello

--- downloadDriver.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-import os
-
-driver_path = os.path.join(os.path.dirname(__file__), "geckodriver.exe")
-
-service = Service(driver_path)
-driver = webdriver.Firefox(service=service)
